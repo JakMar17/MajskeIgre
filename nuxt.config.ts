ed2b588ef6adc266322c5fd13a749b0c3d998398
JakMar17 <jakob.marusic17@gmail.com>
2024-01-13 13:39:09 +0100

    copy of ssvets

---
+++ nuxt.config.ts
@@ -0,0 +1,31 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+    css: [
+        "@/assets/styles/main.scss",
+        "@/node_modules/remixicon/fonts/remixicon.css"
+    ],
+    build: {
+        transpile: [],
+    },
+    vite: {
+        define: {
+            'process.env.DEBUG': true,
+        },
+        server: {
+            hmr: {
+                port: 3008
+            }
+        },
+        css: {
+            preprocessorOptions: {
+                scss: {}
+            }
+        }
+
+    },
+    plugins: [
+    ],
+    modules: [
+        '@nuxt/content',
+    ],
+})

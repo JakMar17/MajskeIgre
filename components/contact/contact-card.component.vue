64bf3a849b201802463f1d562a27c251dce9917a
JakMar17 <jakob.marusic17@gmail.com>
2024-01-13 16:32:20 +0100

    contact page

---
+++ components/contact/contact-card.component.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="wrapper">
+    <img loading="lazy" :src="imageUrl"/>
+    <h2 class="title is-4">{{name}}</h2>
+    <h3 class="subtitle is-6">{{functionTitle}}</h3>
+    <div v-if="phone" class="contact-row">
+      <i class="ri-phone-fill"></i>
+      <span>{{phone}}</span>
+    </div>
+    <div v-if="email" class="contact-row">
+      <i class="ri-mail-fill"></i>
+      <a :href="'mailto:' + email">{{email}}</a>
+    </div>
+  </div>
+</template>
+
+
+<script lang="ts" setup>
+import {MemberModel} from "~/types/models/member.model";
+
+const props = defineProps<MemberModel>();
+
+const {name, functionTitle, phone, email, imageUrl} = toRefs(props);
+
+</script>
+
+
+<style lang="scss" scoped>
+@import "@/assets/styles/main.scss";
+
+.wrapper {
+  @extend .card, .p-2;
+  text-align: center;
+  flex-basis: 25%;
+}
+
+img {
+  height: 15em;
+  width: 15em;
+  object-fit: contain;
+}
+
+.contact-row {
+  display: flex;
+  justify-content: center;
+  gap: 2em;
+}
+</style>
+<script setup lang="ts">
+</script>
---
+++ types/models/member.model.ts
@@ -0,0 +1,7 @@
+export type MemberModel = {
+    name: string;
+    functionTitle: string;
+    email?: string;
+    phone?: string;
+    imageUrl: string;
+}

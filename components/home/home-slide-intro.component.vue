7a19135416adaf55e26616c32bb676cb0049c723
JakMar17 <jakob.marusic17@gmail.com>
2024-01-13 15:31:07 +0100

    landing page

--- components/home/home-slide-intro.component.vue
+++ components/home/home-slide-intro.component.vue
@@ -1,61 +1,78 @@
 <template>
-    <div class="wrapper">
-        <div class="content">
-            <img alt="logo" src="../../assets/images/svgs/logo-sss.svg"/>
-            <h1 class="title">ŠTUDENTSKI SVET STANOVALCEV</h1>
-            <h2 class="subtitle">Študentski dom Ljubljana</h2>
+  <div class="wrapper">
+    <div class="content">
+      <div></div>
+      <div class="content__main">
+        <img id="majske_logo" alt="logo" src="../../assets/images/svgs/logo-majske.svg"/>
 
-            <div class="social">
-                <a href="https://www.facebook.com/profile.php?id=100069419859415" target="_blank" title="Študentski svet stanovalcev"><i class="ri-facebook-circle-fill" style="color: white; font-size: 2em"></i></a>
-                <div class="mx-2 my-4" style="border-left: solid 1px white"></div>
-                <a href="https://www.facebook.com/majskeigre" target="_blank" title="Majske igre"><i class="ri-facebook-circle-fill" style="color: white; font-size: 2em"></i></a>
-                <a href="https://www.instagram.com/majskeigre" target="_blank" title="Majske igre"><i class="ri-instagram-fill" style="color: white; font-size: 2em"></i></a>
-                <a href="https://majske-igre.si" target="_blank" title="Majske igre"><i class="ri-ie-fill" style="color: white; font-size: 2em"></i></a>
-                <div class="mx-2 my-4" style="border-left: solid 1px white"></div>
-                <a href="https://www.facebook.com/pustovanje" target="_blank" title="Študentsko pustovanje v Rožni dolini"><i class="ri-facebook-circle-fill" style="color: white; font-size: 2em"></i></a>
-            </div>
-            <hr/>
-
-            <div class="menu">
-                <NuxtLink to="/sss/kdo-smo">Kdo smo</NuxtLink>
-                <NuxtLink to="/projekti">Projekti</NuxtLink>
-                <NuxtLink to="/blog">Novice</NuxtLink>
-                <NuxtLink to="/kontakt">Kontakt</NuxtLink>
-            </div>
+        <div class="posastko__row">
+          <NuxtLink to="/sport" id="sportko" class="posastko__wrapper">
+            <img class="posastko__img" alt="logo" src="../../assets/images/majske-igre/sportko.svg"/>
+            <h2 class="title is-5 m-0">Šport</h2>
+          </NuxtLink>
+          <NuxtLink to="/zabava" id="zabavko" class="posastko__wrapper">
+            <img class="posastko__img" alt="logo" src="../../assets/images/majske-igre/zabavko.svg"/>
+            <h2 class="title is-5 m-0">Zabava</h2>
+          </NuxtLink>
+          <NuxtLink to="/kultura" id="kulturko" class="posastko__wrapper">
+            <img class="posastko__img" alt="logo" src="../../assets/images/majske-igre/kulturko.svg"/>
+            <h2 class="title is-5 m-0">Kultura</h2>
+          </NuxtLink>
         </div>
-        <img :src="backgroundSrc" alt="background" class="background" loading="lazy"/>
+
+        <div class="menu">
+          <NuxtLink to="/prireditev">O prireditvi</NuxtLink>
+          <NuxtLink to="/kontakt">Organizator & kontakt</NuxtLink>
+        </div>
+
+        <hr/>
+        <div class="social">
+          <a href="https://www.facebook.com/majskeigre" target="_blank" title="Majske igre"><i class="ri-facebook-circle-fill" style="color: white; font-size: 2em"></i></a>
+          <a href="https://www.instagram.com/majskeigre" target="_blank" title="Majske igre"><i class="ri-instagram-fill" style="color: white; font-size: 2em"></i></a>
+        </div>
+
+      </div>
+      <NuxtLink to="https://ssvets.si">
+        <img style="height: 5em" alt="logo" src="../../assets/images/svgs/logo-sss.svg"/>
+      </NuxtLink>
+
+
     </div>
+
+
+    <img :src="backgroundSrc" alt="background" class="background" loading="lazy"/>
+  </div>
 </template>
 
 <script>
 
 export default {
-    async setup() {
-        const {data} = await useAsyncData('slideshow', async () => (await queryContent('slideshow').where({title: 'index'}).findOne()));
-        const imageUrls = data.value.images.map(img => img.image);
-        return {imageUrls}
-    },
-    data() {
-        return {
-            backgroundSrc: '',
-            backgroundSrcIndex: 0,
-            imageUrls: []
-        }
-    },
-    mounted() {
-        if (this.imageUrls.length > 0) {
-            this.backgroundSrc = this.imageUrls[0];
-            this.setImageInterval();
-        }
-    },
-    methods: {
-        setImageInterval() {
-            setInterval(() => {
-                this.backgroundSrcIndex++;
-                this.backgroundSrc = this.imageUrls[this.backgroundSrcIndex % this.imageUrls.length];
-            }, 5000)
-        }
+  async setup() {
+    const {data} = await useAsyncData('slideshow', async () => (await queryContent('slideshow').where({title: 'index'}).findOne()));
+    const imageUrls = data.value.images.map(img => img.image);
+    return {imageUrls}
+  },
+  data() {
+    return {
+      backgroundSrc: '',
+      backgroundSrcIndex: 0,
+      imageUrls: []
     }
+  },
+  mounted() {
+    if (this.imageUrls.length > 0) {
+      this.backgroundSrc = this.imageUrls[0];
+      this.setImageInterval();
+    }
+  },
+  methods: {
+    setImageInterval() {
+      setInterval(() => {
+        this.backgroundSrcIndex++;
+        this.backgroundSrc = this.imageUrls[this.backgroundSrcIndex % this.imageUrls.length];
+      }, 5000)
+    }
+  }
 }
 </script>
 
@@ -63,91 +80,124 @@ export default {
 @import "assets/styles/main";
 
 hr {
-    background: white;
-    width: 75%;
-    margin: 1rem 0;
+  background: white;
+  width: 75%;
+  margin: 1rem 0;
 }
 
 .wrapper {
-    position: relative;
-    z-index: 20;
+  position: relative;
+  z-index: 20;
 }
 
 .content {
-    z-index: 200;
-    position: absolute;
-    background: rgba($neutral, 0.5);
-    height: 100vh;
-    width: 100%;
+  z-index: 200;
+  position: absolute;
+  background: rgba($neutral, 0.75);
+  height: 100vh;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
 
+  &__main {
     flex-direction: column;
-    padding-bottom: 10vh;
     display: flex;
     justify-content: center;
     align-items: center;
+  }
 
-    img {
-        height: 10rem;
-        object-fit: contain;
 
-        &.small {
-            height: 5rem;
-        }
-    }
+  .title {
+    @extend .title, .is-1, .text__has-shadow;
+    text-align: center;
+    color: white;
+    margin-top: 2rem;
+  }
 
-    .title {
-        @extend .title, .is-1, .text__has-shadow;
-        text-align: center;
-        color: white;
-        margin-top: 2rem;
-    }
+  .social {
+    display: flex;
+    gap: 1em;
+  }
+}
 
-    .subtitle {
-        @extend .subtitle, .is-4, .text__has-shadow;
-        text-align: center;
-        color: white;
-    }
-
-    .social {
-        display: flex;
-        gap: 0.25em;
-    }
+#majske_logo {
+  height: 20rem;
+  object-fit: contain;
 }
 
 .background {
-    transition: opacity 0.5s;
-    height: 100vh;
-    width: 100vw;
-    object-fit: cover;
+  transition: opacity 0.5s;
+  height: 100vh;
+  width: 100vw;
+  object-fit: cover;
 }
 
 .menu {
-    display: flex;
-    gap: 1em;
-    flex-wrap: wrap;
-    margin: 0 3vw;
-    justify-content: space-around;
+  display: flex;
+  gap: 2em;
+  flex-wrap: wrap;
+  margin: 0 3vw;
+  justify-content: space-around;
 
-    a {
-        @extend .text__has-shadow;
-        font-size: 1.5em;
-        text-align: center;
-        color: white;
+  a {
+    @extend .text__has-shadow;
+    font-size: 1.5em;
+    text-align: center;
+    color: white;
 
-        text-decoration: underline transparent;
-        transition: text-decoration-color 0.25s ease-in-out;
+    text-decoration: underline transparent;
+    transition: text-decoration-color 0.25s ease-in-out;
 
-        &:hover {
-            text-decoration-color: white;
-        }
+    &:hover {
+      text-decoration-color: white;
     }
+  }
 }
 
 .text {
-    &__has-shadow {
-        text-shadow: 0.15rem 0.15rem 0.25rem rgba(0, 0, 0, 0.75);
+  &__has-shadow {
+    text-shadow: 0.15rem 0.15rem 0.25rem rgba(0, 0, 0, 0.75);
+  }
+}
+
+.posastko {
+  &__wrapper {
+    width: fit-content;
+    transition: transform .4s;
+
+    &:hover {
+      transform: scale(1.5);
+
+      padding: 2em;
+      border-radius: 1em;
     }
+  }
+
+  &__img {
+    height: 8em;
+    width: auto;
+  }
+
+  &__row {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin-bottom: 2em;
+    gap: 2em;
+  }
+}
+
+#kulturko:hover {
+  background: $kultura-dark;
+}
+
+#zabavko:hover {
+  background: $zabava-dark;
+}
+
+#sportko:hover {
+  background: $sport-dark;
 }
 </style>
-<script lang="ts" setup>
-</script>

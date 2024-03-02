c05e4d391d7e86463a3757e9863a7d96a2f45c13
JakMar17 <jakob.marusic17@gmail.com>
2024-03-02 17:45:27 +0100

    ssr default

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    ssr: false,
     css: [
         "@/assets/styles/main.scss",
         "@/node_modules/remixicon/fonts/remixicon.css"

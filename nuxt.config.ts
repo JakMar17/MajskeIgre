aa816dab6b77a29e6ad1afff99906e9d0d839344
JakMar17 <jakob.marusic17@gmail.com>
2024-03-02 17:13:09 +0100

    baseUrl

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+    app: {
+        baseURL: '/MajskeIgre/'
+    },
     css: [
         "@/assets/styles/main.scss",
         "@/node_modules/remixicon/fonts/remixicon.css"

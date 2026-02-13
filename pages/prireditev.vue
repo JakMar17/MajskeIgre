801e1edd491a9c64e3da560cdfbfa8ca680bc0d0
Jakob Marušič <jakob.marusic@dhimahi.com>
2026-02-13 14:37:04 +0100

    feat: prerendering pages

--- pages/prireditev.vue
+++ pages/prireditev.vue
@@ -2,77 +2,50 @@
   <main v-if="descriptionRef">
 
     <div class="flex justify-content--center">
-      <img style="width: 50em" src="../assets/images/svgs/logo-majske.svg"/>
+      <img style="width: 50em" src="../assets/images/svgs/logo-majske.svg" />
     </div>
 
     <div>
 
-      <CardComponent
-          class="mb-4"
-          title="Majske igre – festival študentskih domov"
-          :centered-titles="true"
-          :content="descriptionRef.description"
-      />
+      <CardComponent class="mb-4" title="Majske igre – festival študentskih domov" :centered-titles="true"
+        :content="descriptionRef.description" />
 
-      <CardImageComponent
-          class="sport__wrapper"
-          title="Šport"
-          :image-url="descriptionRef.sportImage"
-          :content="descriptionRef.sportDescription"
-      :links="[links.sport]">
+      <CardImageComponent class="sport__wrapper" title="Šport" :image-url="descriptionRef.sportImage"
+        :content="descriptionRef.sportDescription" :links="[links.sport]">
         <template v-slot:title>
           <div class="flex justify-content--between align-items--baseline">
             <h1 class="title is-2">Šport</h1>
-            <img style="height: 8em" src="../assets/images/majske-igre/sportko.svg"/>
+            <img style="height: 8em" src="../assets/images/majske-igre/sportko.svg" />
           </div>
         </template>
       </CardImageComponent>
 
-      <CardImageComponent
-          class="culture__wrapper"
-          title="Kultura"
-          :image-url="descriptionRef.cultureImage"
-          :content="descriptionRef.cultureDescription"
-          :links="[links.culture]"
-      >
+      <CardImageComponent class="culture__wrapper" title="Kultura" :image-url="descriptionRef.cultureImage"
+        :content="descriptionRef.cultureDescription" :links="[links.culture]">
         <template v-slot:title>
           <div class="flex justify-content--between align-items--baseline">
             <h1 class="title is-2">Kultura</h1>
-            <img style="height: 8em" src="../assets/images/majske-igre/kulturko.svg"/>
+            <img style="height: 8em" src="../assets/images/majske-igre/kulturko.svg" />
           </div>
         </template>
       </CardImageComponent>
 
-      <CardImageComponent
-          class="concert__wrapper"
-          title="Zabava"
-          :image-url="descriptionRef.concertImage"
-          :content="descriptionRef.concertDescription"
-          :links="[links.concerts]"
-      >
+      <CardImageComponent class="concert__wrapper" title="Zabava" :image-url="descriptionRef.concertImage"
+        :content="descriptionRef.concertDescription" :links="[links.concerts]">
         <template v-slot:title>
           <div class="flex justify-content--between align-items--baseline">
             <h1 class="title is-2">Zabava</h1>
-            <img style="height: 8em" src="../assets/images/majske-igre/zabavko.svg"/>
+            <img style="height: 8em" src="../assets/images/majske-igre/zabavko.svg" />
           </div>
         </template>
       </CardImageComponent>
     </div>
 
-    <CardComponent
-        class="mt-6"
-        title="Organizator Majskih iger - Študentski svet stanovalcev"
-        :centered-titles="true"
-        :content="descriptionRef.organizer"
-    />
+    <CardComponent class="mt-6" title="Organizator Majskih iger - Študentski svet stanovalcev" :centered-titles="true"
+      :content="descriptionRef.organizer" />
 
-    <CardImageComponent
-      title="Organizacijska ekipa"
-      :image-url="descriptionRef.teamImage"
-      :content="descriptionRef.teamDescription"
-      :links="[contactLink]"
-      :reversed="true"
-    />
+    <CardImageComponent title="Organizacijska ekipa" :image-url="descriptionRef.teamImage"
+      :content="descriptionRef.teamDescription" :links="[contactLink]" :reversed="true" />
 
   </main>
 
@@ -80,8 +53,8 @@
 
 <script lang="ts" setup>
 
-import {MajskeDescriptionModel} from "~/models/majske-description.model";
-import {createSeoFunction} from "~/functions/create-seo.function";
+import { MajskeDescriptionModel } from "~/models/majske-description.model";
+import { createSeoFunction } from "~/functions/create-seo.function";
 
 const links = {
   concerts: {
@@ -109,17 +82,15 @@ const contactLink = {
 
 const descriptionRef = ref<MajskeDescriptionModel | null>(null);
 
-useAsyncData('fetchDescriptions', () => queryContent<MajskeDescriptionModel>('descriptions/event').findOne())
-    .then(({data}) => {
-      descriptionRef.value = data.value;
-      if(data.value != null) {
-        createSeoFunction({
-          title: "Majske igre",
-          description: data.value?.description,
-          imageUrl: data.value?.concertImage
-        });
-      }
-    });
+const { data: contentData } = await useAsyncData('fetchDescriptions', () => queryContent<MajskeDescriptionModel>('descriptions/event').findOne());
+descriptionRef.value = contentData.value;
+if (contentData.value != null) {
+  createSeoFunction({
+    title: "Majske igre",
+    description: contentData.value?.description,
+    imageUrl: contentData.value?.concertImage
+  });
+};
 
 </script>
 
@@ -143,5 +114,4 @@ useAsyncData('fetchDescriptions', () => queryContent<MajskeDescriptionModel>('de
     background: $zabava-primary;
   }
 }
-
 </style>
